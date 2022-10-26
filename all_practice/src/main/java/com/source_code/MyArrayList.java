a81d5b5a6f5f21c7ee43178968d294087df5d9a9
muxiaoling <tjh86mu@gmail.com>
2022-10-26 13:07:28 +0800

    删除冗余代码

--- all_practice/src/main/java/com/source_code/MyArrayList.java
+++ all_practice/src/main/java/com/source_code/MyArrayList.java
@@ -1,28 +1,8 @@
 package com.source_code;
 
-import java.util.*;
-
 public class MyArrayList {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            list.add(i);
-        }
-        list.get(0);
-        char[] ch = new char[1];
-        System.out.print(ch[0]);
-        List<Integer> li = new ArrayList<>(list);
 
-
-        Collections.sort(list);
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(list.get(i));
-//        }
-
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
-        }
     }
 
 }
--- all_practice/src/main/java/com/source_code/MyDeque.java
+++ all_practice/src/main/java/com/source_code/MyDeque.java
@@ -1,12 +1,7 @@
 package com.source_code;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class MyDeque {
     public static void main(String[] args) {
-        Deque<Integer> deque1 = new LinkedList<>();
-        Deque<Integer> deque2 = new ArrayDeque<>();
+
     }
 }
